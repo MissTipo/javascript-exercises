1bbe7f055921248c88d68405d9d3c4db1321077f
MissTipo <dorine.a.tipo@gmail.com>
2023-11-20 09:41:30 +0300

    Return the sum of every number between 2 integers inclusive

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,30 @@
-const sumAll = function() {
-
+sumAll = function(arg1, arg2) {
+  if (
+    typeof arg1 != "number" ||
+    typeof arg2 != "number" ||
+    arg1 < 0 ||
+    arg2 < 0
+  ) {
+    return "ERROR";
+  }
+  let start = 0;
+  let end = 0;
+  if (arg1 > arg2) {
+    start = arg2;
+    end = arg1;
+  } else if (arg1 < arg2) {
+    start = arg1;
+    end = arg2;
+  } else {
+    return arg1;
+  }
+  sum = 0;
+  for (let i = start; i <= end; i += 1) {
+    sum += i;
+  }
+  return sum;
 };
 
+console.log(sumAll(1, 4));
 // Do not edit below this line
 module.exports = sumAll;
