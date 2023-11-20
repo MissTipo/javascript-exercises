c5dcc051851f01b440692f595b48484a69dd99c1
MissTipo <dorine.a.tipo@gmail.com>
2023-11-20 09:40:05 +0300

    Remove an element from an array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,15 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...num) {
+  const newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (num.includes(arr[i])) {
+      // Do nothing
+    } else {
+      newArr.push(arr[i]);
+    }
+  }
+  return newArr;
 };
 
+removeFromArray([1, 3, 6], 6, 1);
 // Do not edit below this line
 module.exports = removeFromArray;
