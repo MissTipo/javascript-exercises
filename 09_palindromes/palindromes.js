a5c6c71cf93eb0d619cac72aeee091dbe5e50cb5
MissTipo <dorine.a.tipo@gmail.com>
2023-11-27 13:42:19 +0300

    Add: Palindrome function
    determine whether or not a given string is a palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function(str) {
+  let str1 = str.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+  let pal = str1.split("").reverse().join("");
+  if (pal === str1) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
