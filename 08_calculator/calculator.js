bb0ab377530f0296742b718c428dba1fb455904d
MissTipo <dorine.a.tipo@gmail.com>
2023-11-27 13:39:44 +0300

    Create a calculator function
    add, subtract, get the sum, multiply, get the power, and find the factorial

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,33 +1,45 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let _sum = 0;
+	for (let i = 0; i < arr.length; i++) {
+		_sum += arr[i];
+	}
+	return _sum;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+	let product = 1;
+	for (let i = 0; i < arr.length; i++) {
+		product *= arr[i];
+	}
+	return product;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	if (a == 0 || a == 1) {
+		return 1;
+	} else {
+		return a * factorial(a - 1);
+	}
 };
 
 // Do not edit below this line
 module.exports = {
-  add,
-  subtract,
-  sum,
-  multiply,
-  power,
-  factorial
+	add,
+	subtract,
+	sum,
+	multiply,
+	power,
+	factorial,
 };
